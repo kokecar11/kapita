539a7ae358c4e5d022069af2f52c93f6739c5b66
Koke Carpintero <kokecar11.dev@gmail.com>
2023-05-06 01:53:42 -0500

    fix(kapita-ui): added size and variant in button component

--- packages/kapita-ui/src/components/button/Button.tsx
+++ packages/kapita-ui/src/components/button/Button.tsx
@@ -1,11 +1,16 @@
-import {type QwikIntrinsicElements, component$, useStyles$, JSXChildren, Slot } from '@builder.io/qwik';
+import {type QwikIntrinsicElements, component$, useStyles$, Slot } from '@builder.io/qwik';
 import stylesButton from './Button.css?inline'
 export type ButtonProps = QwikIntrinsicElements['button'] & {
-  children?: JSXChildren;
+  variant?: 'primary' | 'secondary' | 'danger' | 'info' | 'warning' | 'success';
+  size?: 'small' | 'medium' | 'large';
 };
 
-export const Button = component$( ({...props}: ButtonProps) => {
+export const Button = component$( ({variant, size,...props}: ButtonProps) => {
   useStyles$(stylesButton)
-    return <button {...props}><Slot /></button>
+    return (
+      <button {...props} class={`btn btn-${variant ? variant:'primary'} btn-${size ? size : 'medium'}`}>
+        <Slot />
+      </button>
+    );
   }
 );
